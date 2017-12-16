789d4fd43ccf071b0417d220d702f58d94924a03
Ravart <me@ravart.cc>
2017-12-16 18:28:21 +0100

    Presents Damage Model added (with Sound Effects)

---
+++ Assets/Scripts/Damiano/Destructible.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destructible : MonoBehaviour {
+
+    public GameObject UI_Damage;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    protected void SufferDamage(int damage)
+    {
+        GameObject dmg = Instantiate(UI_Damage, transform.position, Quaternion.identity);
+        dmg.GetComponent<UI_Damage>().damage = damage;
+    }
+}
---
+++ Assets/Scripts/Damiano/Present.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource)), SelectionBase]
+public class Present : Destructible {
+
+    public int health = 100;
+    public int points = 5;
+
+    public AudioClip clip;
+    AudioSource audioSource;
+
+    // Use this for initialization
+    void Awake() {
+        audioSource = GetComponent<AudioSource>();
+        audioSource.clip = clip;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnCollisionEnter(Collision collision)
+    {
+        foreach (ContactPoint contact in collision.contacts)
+        {
+            Debug.DrawRay(contact.point, contact.normal, Color.white);
+        }
+        if (collision.relativeVelocity.magnitude > 2) {
+            SufferDamage(Mathf.RoundToInt(collision.relativeVelocity.magnitude) * points);
+            audioSource.Play();
+        }
+    }
+}
